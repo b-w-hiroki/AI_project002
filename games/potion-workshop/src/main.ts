eb4fd2b0852c446bd90575aac2a59c1b361c2c2a
B.W-Hiroki <kojack1221@gmail.com>
2026-09-11 22:54:46 +0900

    fix(potion): install wall-clock autosave safeguard

--- games/potion-workshop/src/main.ts
+++ games/potion-workshop/src/main.ts
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import { initCrazyGames } from "./platform/crazygames";
+import { installWallClockPersistence } from "./persistence";
 import { installPotionPresentation } from "./presentation";
 import { IdleScene } from "./scenes/IdleScene";
 
@@ -39,6 +40,7 @@ Phaser.GameObjects.Container.prototype.setInteractive = function (
 // CrazyGames ポータル上でのみ SDK が有効化される（他環境では no-op）
 void initCrazyGames();
 installPotionPresentation();
+installWallClockPersistence();
 
 new Phaser.Game({
   type: Phaser.AUTO,
